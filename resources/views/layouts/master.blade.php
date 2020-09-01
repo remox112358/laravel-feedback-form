640db7aa9cdb39cad8a0148824a2136fd724c0c4
remox112358 <remox112358@gmail.com>
2020-09-01 16:55:42 +0400

    Update header and add fontawesome kit local

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,7 +18,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/jquery-3.5.1.min.js') }}" defer></script>
     <script src="{{ asset('js/bootstrap.bundle.min.js') }}" defer></script>
-    <script src="https://kit.fontawesome.com/0cbe7f2905.js" crossorigin="anonymous" defer></script>
+    <script src="{{ asset('js/fontawesome-kit.js') }}" defer></script>
     <script src="{{ asset('js/script.js') }}" defer></script>
 </head>
 <body>
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -3,10 +3,17 @@
         <div class="container">
             <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
             <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <li class="nav-item">
+                        <div class="social-links">
+                            <a href="https://github.com/remox112358/laravel-feedback-form" class="link" target="_blank"><i class="fab fa-github"></i></a>
+                        </div>
+                    </li>
+                </div>
                 <ul class="navbar-nav ml-auto">
                     @if (Auth::check())
                         <li class="nav-item">
-                            <a href="{{ route('auth.signout') }}" class="nav-link">Выйти</a>
+                            <a class="reset" href="{{ route('auth.signout') }}" class="nav-link">Выйти</a>
                         </li>
                     @endif
                 </ul>
